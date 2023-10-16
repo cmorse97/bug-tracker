1d9daee90d0c5db675d8bfbc8be5a0c74c658492
Caleb Morse <calebmorse.code@gmail.com>
2023-10-16 17:36:31 -0500

    Add async to controller

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,30 +1,39 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get projects
 // @route GET /api/projects
 // @access Private
-const getProjects = (req, res) => {
+const getProjects = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: 'Get projects' });
-};
+});
 
 // @desc Set project
 // @route POST /api/projects
 // @access Private
-const setProject = (req, res) => {
+const setProject = asyncHandler(async (req, res) => {
+	if (!req.body.text) {
+		res.status(400);
+		throw new Error('Please add text field');
+	}
+
+	console.log(req.body.text);
+
 	res.status(200).json({ message: 'Create project' });
-};
+});
 
 // @desc Update project by id
 // @route PUT /api/projects/:id
 // @access Private
-const updateProject = (req, res) => {
+const updateProject = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Update project ${req.params.id}` });
-};
+});
 
 // @desc Delete project by id
 // @route DELETE /api/projects/:id
 // @access Private
-const deleteProject = (req, res) => {
+const deleteProject = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: `Delete project ${req.params.id}` });
-};
+});
 
 module.exports = {
 	getProjects,
